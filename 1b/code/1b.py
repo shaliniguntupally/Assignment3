5da7c1c176d9b3872372e884d2c8c85e06ba637b
shaliniguntupally <146143130+shaliniguntupally@users.noreply.github.com>
2023-11-26 04:28:08 +0530

    Add files via upload

---
+++ 1b/code/1b.py
@@ -0,0 +1,14 @@
+# b) 98th Percentile of BMI
+sample_98th_percentile_bmi = np.percentile(sample['BMI'], 98)
+population_98th_percentile_bmi = np.percentile(data['BMI'], 98)
+
+# Create comparison charts
+plt.figure(figsize=(10, 6))
+sns.kdeplot(sample['BMI'], label='Sample BMI')
+sns.kdeplot(data['BMI'], label='Population BMI')
+plt.axvline(sample_98th_percentile_bmi, color='blue', linestyle='dashed', label='Sample 98th Percentile')
+plt.axvline(population_98th_percentile_bmi, color='orange', linestyle='dashed', label='Population 98th Percentile')
+plt.title('Comparison of BMI 98th Percentile')
+plt.xlabel('BMI')
+plt.legend()
+plt.show()
